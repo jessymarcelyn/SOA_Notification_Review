7e95817f95cd1689c20667c06300ffe4d0759595
benita <109541073+benit11@users.noreply.github.com>
2024-06-20 19:02:29 +0700

    Update gateway.py

--- api/gateway.py
+++ api/gateway.py
@@ -305,60 +305,50 @@ class GatewayService:
     #GOPAY
     gopay_rpc = RpcProxy('gopay_service')
 
-    # @http('GET', '/gopay/no_telp/<string:no_telp>')
-    # def get_no_telp(self, request, no_telp):
-    #     pembayaran = self.ovo_rpc.get_no_telp()
-    #     return json.dumps(pembayaran)
-    
     @http('POST', '/gopay') #cek nomor telepon, return idtransaksi
-    def post_pembayaran(self, request):
+    def post_pembayaran_gopay(self, request):
         data = json.loads(request.get_data(as_text=True))
         pembayaran = self.gopay_rpc.insert_transaksi(data['no_telp'], data['nominal'])
         return json.dumps(pembayaran)
          
     @http('GET', '/gopay/status/<string:id_transaksi>') #return status pembayaran
-    def get_status_pembayaran_by_id_transaksi(self, request, id_transaksi):
+    def get_status_pembayaran_by_id_transaksi_gopay(self, request, id_transaksi):
         pembayaran = self.gopay_rpc.get_status_transaksi(id_transaksi)
         return json.dumps(pembayaran)
     
     @http('PUT', '/gopay/bayar') #check pin dan check saldo
-    def bayar(self, request):
+    def bayar_gopay(self, request):
         data = json.loads(request.get_data(as_text=True))
         pembayaran = self.gopay_rpc.bayar(data['id_transaksi'], data['pin'])
         return json.dumps(pembayaran)
     
     @http('GET', '/gopay/timestamp/<string:id_transaksi>')
-    def get_timestamp_by_id_transaksi(self, request, id_transaksi):
+    def get_timestamp_by_id_transaksi_gopay(self, request, id_transaksi):
         timestamp = self.gopay_rpc.get_timestamp(id_transaksi)
         return json.dumps(timestamp)
     
     # OVO
     ovo_rpc = RpcProxy('ovo_service')
 
-    # @http('GET', '/ovo/no_telp/<string:no_telp>')
-    # def get_no_telp(self, request, no_telp):
-    #     pembayaran = self.ovo_rpc.get_no_telp()
-    #     return json.dumps(pembayaran)
-    
     @http('POST', '/ovo') #cek nomor telepon, return idtransaksi
-    def post_pembayaran(self, request):
+    def post_pembayaran_ovo(self, request):
         data = json.loads(request.get_data(as_text=True))
         pembayaran = self.ovo_rpc.insert_transaksi(data['no_telp'], data['nominal'])
         return json.dumps(pembayaran)
          
     @http('GET', '/ovo/status/<string:id_transaksi>') #return status pembayaran
-    def get_status_pembayaran_by_id_transaksi(self, request, id_transaksi):
+    def get_status_pembayaran_by_id_transaksi_ovo(self, request, id_transaksi):
         pembayaran = self.ovo_rpc.get_status_transaksi(id_transaksi)
         return json.dumps(pembayaran)
     
     @http('PUT', '/ovo/bayar') #check pin dan check saldo
-    def bayar(self, request):
+    def bayar_ovo(self, request):
         data = json.loads(request.get_data(as_text=True))
         pembayaran = self.ovo_rpc.bayar(data['id_transaksi'], data['pin'])
         return json.dumps(pembayaran)
     
     @http('GET', '/ovo/timestamp/<string:id_transaksi>')
-    def get_timestamp_by_id_transaksi(self, request, id_transaksi):
+    def get_timestamp_by_id_transaksi_ovo(self, request, id_transaksi):
         timestamp = self.ovo_rpc.get_timestamp(id_transaksi)
         return json.dumps(timestamp)
     
